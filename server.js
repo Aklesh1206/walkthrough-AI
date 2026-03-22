a02676e38b8638a30eb12b8915d6f4c0a3592292
Aklesh1206 <aklesh.swain@gmail.com>
2026-03-22 06:34:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.json({ limit: '50kb' }));
 
 // Serve the frontend HTML directly from this folder
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/v4_final.html');
+  res.sendFile(__dirname + '/v5_final.html');
 });
 app.use(express.static(__dirname));
 
